44bac59f503aba1c964db6baa81d705f5cd15c98
ajay <ajay@ajayraj.co>
2020-11-17 04:05:37 -0800

    Add custom template tag for file format

---
+++ yavs/templatetags/title_format.py
@@ -0,0 +1,10 @@
+from django import template
+from .models import Video
+import os
+
+register = template.Library()
+
+@register.simple_tag
+def tb_path(video_path):
+    file, extension = os.path.splitext(video_path)
+    return file + "_tb.png"
