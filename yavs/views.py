ab661ce60af9f2d6e2bfc6564cf692dbc4286b20
Ajay Raj <ajay@ajayraj.co>
2020-11-17 12:38:05 +0000

    Fixed thumbnail generation

--- yavs/views.py
+++ yavs/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from .models import Video, Comment
 from pathlib import Path
 from django.conf import settings
-from .tasks import generate_insights_for_video
+from .tasks import generate_insights_for_video, generate_thumbnail
 import string, random, os, re
 
 #For development
